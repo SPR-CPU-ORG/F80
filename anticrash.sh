8fd45addda8c292fe4861c7287151ac127ae89f7
Reload-Life(SilenT) <reload-life@users.noreply.github.com>
2017-04-04 21:15:41 +0430

    Update anticrash.sh

--- anticrash.sh
+++ anticrash.sh
@@ -1,5 +1,13 @@
 cd $home/F80
 figlet Running
 figlet SprCpu Company
-screen -d -m ./Cli.sh
-screen -d -m ./Api.sh
+while true; do
+  tmux kill-session -t F80-CLI
+  tmux new-session -d -s F80-CLI "./Cli.sh"
+  tmux detach -s Cli.sh
+  tmux kill-session -t F80-API
+  tmux new-session -d -s F80-API "./Api.sh"
+  tmux detach -s Api.sh
+  figlet Bots Are Running !
+sleep 100000
+done
