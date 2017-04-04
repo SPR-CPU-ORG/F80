7e95cc9ab9dcc21184326225f71af8e8855d2ca0
Reload-Life(SilenT) <reload-life@users.noreply.github.com>
2017-04-04 19:49:17 +0430

    Simple Bashes to Run Bots !

---
+++ Cli.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+install_luarocks() {
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.2.1 # Current stable
+
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
+
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  make build && make install
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting.";exit $RET;
+  fi
+
+  cd ..
+  rm -rf luarocks
+}
+
+install_rocks() {
+
+  ./.luarocks/bin/luarocks-5.2 install luasec
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks-5.2 install luasocket
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+  
+  ./.luarocks/bin/luarocks-5.2 install redis-lua
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+}
+
+if [ "$1" = "install" ]; then
+  install_luarocks
+  install_rocks
+else
+  while true; do
+    ./Bot/Telegram-cli/Telegram-Cli -p Moshtari -s ./Bot/Bot.lua
+    sleep 5s
+  done
+fi
