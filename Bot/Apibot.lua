2444480e98afda7cbef5a3304280fc9d705ce6e6
Reload-Life(SilenT) <reload-life@users.noreply.github.com>
2017-04-04 21:57:03 +0430

    Update Apibot.lua

--- Bot/Apibot.lua
+++ Bot/Apibot.lua
@@ -4,6 +4,7 @@ package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
   .. ';.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 --
+  _Config = dofile('./Bot/Config.lua')
 -------------- Packages
 function RedisDb()
 local Redis = require 'redis'
